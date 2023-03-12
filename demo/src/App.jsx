b27c1751cf3d6dc1f1bacfe7207d354eed2ebac4
Milan Hauth <milahu@gmail.com>
2023-03-12 09:33:23 +0100

    update demo

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import styles from './App.module.css';
+
+import MonacoEditor from '../../MonacoEditor.jsx'
+
+const value = `\
+# xclip.nix
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/misc/xclip/default.nix
+
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoreconfHook
+, libXmu
+}:
+
+stdenv.mkDerivation rec {
+  pname = "xclip";
+  version = "0.13";
+
+  src = fetchFromGitHub {
+    owner = "astrand";
+    repo = "xclip";
+    rev = version;
+    sha256 = "0q0hmvcjlv8arhh1pzhja2wglyj6n7z209jnpnzd281kqqv4czcs";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  buildInputs = [ libXmu ];
+
+  meta = {
+    description = "Tool to access the X clipboard from a console application";
+    homepage = "https://github.com/astrand/xclip";
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.all;
+  };
+}
+`
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class={styles.App} style=" ">
+      {
+        // header
+      }
+      <div>
+        live demo for <a href="https://github.com/milahu/solidjs-monaco-editor-component">solidjs-monaco-editor-component</a>
+      </div>
+      {
+        // main
+      }
+      <div class={styles.editor}>
+        <MonacoEditor
+          options={{
+            value,
+            minimap: {
+              enabled: false,
+            },
+          }}
+        />
+      </div>
+      {
+        // footer
+      }
+      <div>
+        some footer
+      </div>
     </div>
   );
 }
--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,12 +1,51 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
+// TODO https://stackoverflow.com/questions/73483816/minimal-monaco-editor-without-language-support
+
+// https://github.com/vdesjs/vite-plugin-monaco-editor
+//import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+
+const assetsDir = '';
+//const assetsDir = 'assets/';
+
+const outputDefaults = {
+  // remove hashes from filenames
+  entryFileNames: `${assetsDir}[name].js`,
+  chunkFileNames: `${assetsDir}[name].js`,
+  assetFileNames: `${assetsDir}[name].[ext]`,
+}
+
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
+  plugins: [
+    solidPlugin(),
+    //monacoEditorPlugin({
+    //  languageWorkers: [],
+    //}),
+  ],
+  base: "./", // generate relative paths in html
+  //root: "..", // folder of index.html
   build: {
+    outDir: '../docs', // github pages
+    emptyOutDir: true,
     target: 'esnext',
+    //sourcemap: true,
+    //minify: false, // smaller git diffs
+    rollupOptions: {
+      output: {
+        ...outputDefaults,
+      }
+    },
   },
+  esbuild: {
+    keepNames: true,
+  },
+  worker: {
+    rollupOptions: {
+      output: {
+        ...outputDefaults,
+      }
+    },
+  },
+  clearScreen: false,
 });
